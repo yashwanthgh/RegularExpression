245d34957cecfda8add7d8d7a635d442c6eaf51c
yashwanthgh <yugnasura@gmail.com>
2024-03-22 21:19:05 +0530

    Added: Functionality to check weather the email format is valid

--- RegularExp.cs
+++ RegularExp.cs
@@ -11,25 +11,45 @@ namespace RegularExpression
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter yout name: ");
+            Console.WriteLine("Enter your name: ");
             string? name = Console.ReadLine();
-            if(name != null ) 
+            Console.WriteLine("Enetr your email: ");
+            string? email = Console.ReadLine();
+            if (name != null && email != null)
             {
                 EnterName(name);
+                EnterEmail(name);
             }
         }
         public static void EnterName(string name)
         {
             string pattern = "^[A-Z]{1}[a-z]{2,19}\\s[A-Z]{1}[a-z]{2,19}$";
-            
+
             Regex regex = new(pattern);
-            if(regex.IsMatch(name))
+            if (regex.IsMatch(name))
             {
                 Console.WriteLine($"Your name is {name}");
-            } else
+            }
+            else
             {
                 Console.WriteLine("First name first character should be capital \nSecound name first character should be capital \nAnd both name must be more than 3 and less than 20 character long");
             }
         }
+
+        public static void EnterEmail(string email)
+        {
+            string pattern = "^[a-z0-9.]+@[a-z0-9.]+\\.[a-z]+$";
+            Regex regex = new(pattern);
+            if (regex.IsMatch(email))
+            {
+                Console.WriteLine($"Your email is {email}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email formate");
+            }
+        }
+
+
     }
 }
